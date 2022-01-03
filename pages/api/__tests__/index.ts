2d58147465beb54eb48806bb576b5ec66cc815f7
Dias Junior <reinaldo.diasjunior@gmail.com>
2022-01-03 23:11:20 +0100

    add test cases

--- pages/api/__tests__/index.ts
+++ pages/api/__tests__/index.ts
@@ -1,33 +1,73 @@
+import request from "supertest";
+import http, { IncomingMessage, ServerResponse } from "http";
+import { apiResolver } from "next/dist/server/api-utils";
+import handler from "../articles/index";
 
-import request from 'supertest'
-import http, { IncomingMessage, ServerResponse } from 'http'
-import { apiResolver } from 'next/dist/server/api-utils'
-import handler from "../articles/index"
+process.env.ECONOMIST_URL = "https://www.economist.com/";
 
-process.env.ECONOMIST_URL = "https://www.economist.com/"
-process.env.ECONOMIST_URL = "teste"
-
-describe("should return items", ()=>{
-
+describe("should return items", () => {
   const apiPreviewPropsMock = {
-    previewModeId: 'id',
-    previewModeEncryptionKey: 'key',
-    previewModeSigningKey: 'key',
-  }
+    previewModeId: "id",
+    previewModeEncryptionKey: "key",
+    previewModeSigningKey: "key",
+  };
 
   const requestListener = (req: IncomingMessage, res: ServerResponse) => {
-    apiResolver(req, res, undefined, handler, apiPreviewPropsMock, false)
-  }
+    apiResolver(req, res, undefined, handler, apiPreviewPropsMock, false);
+  };
 
-  it('Should return 200 to a valid request', async () => {
-    const server = http.createServer(requestListener)
-    const response = await request
-      .agent(server)
-      .get('/api/articles')
+  const headlinesCount = 7;
+    const discoversCount = 6;
+    const favoritesCount = 5;
+    const sectionsCount = 8;
+    const sectionsListCount = 3
+
+  it("Should return all the elements the home page expects", async () => {
+    const server = http.createServer(requestListener);
+    const response = await request.agent(server).get("/api/articles");
+
+    ;
 
 
-    expect(response.statusCode).toEqual(200)
-    server.close()
-  })
+    expect(response.body.headlines.length).toEqual(headlinesCount);
+    expect(response.body.discovers.length).toEqual(discoversCount);
+    expect(response.body.favorites.length).toEqual(favoritesCount);
+    expect(response.body.sections.length).toEqual(sectionsCount);
 
-})
+    for (let i = 0; i < sectionsCount; i++) {
+      console.log("passou: ", response.body.sections[i].list)
+      expect(response.body.sections[i].list.length).toEqual(sectionsListCount);
+    }
+    server.close();
+  });
+
+  it("Should return href and title for all elements", async () => {
+    const server = http.createServer(requestListener);
+    const response = await request.agent(server).get("/api/articles");
+
+    for (let i = 0; i < headlinesCount; i++) {
+      expect(response.body.headlines[i].href).toBeDefined();
+      expect(response.body.headlines[i].title).toBeDefined();
+    }
+
+    for (let i = 0; i < discoversCount; i++) {
+      expect(response.body.discovers[i].href).toBeDefined();
+      expect(response.body.discovers[i].title).toBeDefined();
+    }
+
+    for (let i = 0; i < favoritesCount; i++) {
+      expect(response.body.favorites[i].href).toBeDefined();
+      expect(response.body.favorites[i].title).toBeDefined();
+    }
+
+    for (let i = 0; i < sectionsCount; i++) {
+      expect(response.body.sections[i].section).toBeDefined();
+      for (let j = 0; j < sectionsListCount; j++) {
+        expect(response.body.sections[i].list[j]).toBeDefined();
+      }
+    }
+
+
+    server.close();
+  });
+});
