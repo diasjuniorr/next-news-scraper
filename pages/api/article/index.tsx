01d23ea7e3d63faeb4126822bb08d65ab15f9ac4
Dias Junior <reinaldo.diasjunior@gmail.com>
2022-01-05 23:39:09 +0100

    format api article route

--- pages/api/article/index.tsx
+++ pages/api/article/index.tsx
@@ -2,58 +2,57 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import axios from "axios";
 import * as cheerio from "cheerio";
 
-
 interface ArticleResponse {
   title: string;
   paragraphs: string[];
 }
 
-
 interface Err {
-    error: string;
+  error: string;
 }
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ArticleResponse | Err>
 ) {
-  return new Promise<void>( async (resolve, reject) => {
-      const {method } = req
-  
-  if (method !== "POST") {
-    res.status(404).json({ error: "Method not allowed" });
-    return;
-  }
-  
-  let {slug} = await req.body
-  slug = decodeQuestionMark(decodeSlash(slug))
-  const paragraphs: string[] = []
-  
-  try {
-    axios(`https://www.economist.com${slug}`).then((response) => {
-      const data = response.data;
-      const $ = cheerio.load(data);
+  return new Promise<void>(async (resolve, reject) => {
+    const { method } = req;
 
-      const title = $("span.article__headline").text();
+    if (method !== "POST") {
+      res.status(404).json({ error: "Method not allowed" });
+      return;
+    }
 
-      $("p.article__body-text", data).each((idx, el) => {
-        paragraphs.push($(el).text())
-      })
+    let { slug } = await req.body;
+    slug = decodeQuestionMark(decodeSlash(slug));
+    const paragraphs: string[] = [];
 
-    return res.status(200).json({title,paragraphs});
-    resolve()
-    }).catch((err) => {
-      console.log("ERROR", err)
-      res.status(404).json({ error: "Not found" });
-      reject()
-    });
-  } catch (error) {
-    console.log(error);
-     res.status(500).json({ error: "Internal Server Error" });
-     resolve()
-  }
+    try {
+      axios(`https://www.economist.com${slug}`)
+        .then((response) => {
+          const data = response.data;
+          const $ = cheerio.load(data);
 
-  })
+          const title = $("span.article__headline").text();
+
+          $("p.article__body-text", data).each((idx, el) => {
+            paragraphs.push($(el).text());
+          });
+
+          return res.status(200).json({ title, paragraphs });
+          resolve();
+        })
+        .catch((err) => {
+          console.log("ERROR", err);
+          res.status(404).json({ error: "Not found" });
+          reject();
+        });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ error: "Internal Server Error" });
+      resolve();
+    }
+  });
 }
 
 function decodeSlash(url: string) {
