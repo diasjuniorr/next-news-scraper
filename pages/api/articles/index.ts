157240efe92def30cd9f4736680203847afac584
Dias Junior <reinaldo.diasjunior@gmail.com>
2022-01-17 13:24:01 +0100

    add methods filter

--- pages/api/articles/index.ts
+++ pages/api/articles/index.ts
@@ -24,6 +24,11 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ArticlesResponse | Err>
 ) {
+  const { method } = req;
+  if (method !== "GET") {
+    return res.status(405).json({ error: "Method not allowed" });
+  }
+  
   try {
     const articles = await getArticles();
     return res.status(200).json(articles);
